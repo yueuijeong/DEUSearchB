e798e9f2412be344159747e4c8035d21fb7c00ed
yeongwoon <fly82283@gmail.com>
2017-11-16 14:05:35 +0900

    Initial Commit

---
+++ DEUSearchBTests/DEUSearchBTests.swift
@@ -0,0 +1,36 @@
+//
+//  DEUSearchBTests.swift
+//  DEUSearchBTests
+//
+//  Created by D7703_21 on 2017. 11. 16..
+//  Copyright © 2017년 D7703_21. All rights reserved.
+//
+
+import XCTest
+@testable import DEUSearchB
+
+class DEUSearchBTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
